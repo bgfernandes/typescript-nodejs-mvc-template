b6510dac70bae895158a468d85d1beb40a717b5b
Bruno Fernandes <bruno.gabriel.fernandes@gmail.com>
2021-06-25 02:43:40 +0800

    Adds lint rule to enforce quote use consistency

--- test/controllers/homeController.test.ts
+++ test/controllers/homeController.test.ts
@@ -3,13 +3,13 @@ import app from './../../src/app';
 
 describe('index', () => {
   it('renders the index page', async () => {
-    const response = await request(app).get("/");
+    const response = await request(app).get('/');
     expect(response.status).toBe(200);
     expect(response.text).toMatchSnapshot();
   });
 
   it('renders the index page localized', async () => {
-    const response = await request(app).get("/?lang=pt-br");
+    const response = await request(app).get('/?lang=pt-br');
     expect(response.status).toBe(200);
     expect(response.text).toMatchSnapshot();
   });
