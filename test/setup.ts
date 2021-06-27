427072ff231b6e1829730336dd46820f59d296af
Bruno Fernandes <bruno.gabriel.fernandes@gmail.com>
2021-06-27 11:39:50 +0800

    Updates comments in the test setup file and readme

--- test/setup.ts
+++ test/setup.ts
@@ -3,7 +3,8 @@
   This will truncate every table in the database to make sure every test case has a clean DB.
 
   Decided to go with this approach, initially I tried doing active_record/ecto style by running every test inside
-  it's own transaction, but it is not as easy as I expected.
+  it's own transaction, but it is not as easy as I expected. It could be done but it would require some boiler plate
+  code to be added to each test file, that I don't want to.
 
   Also, to make sure that no test case will contaminate the DB to another test case, they need to run sequentially,
   with running jest in --runInBand mode.
