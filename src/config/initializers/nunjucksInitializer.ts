650f2342e4b961d6202d8c7701774618e378a7c2
Bruno Fernandes <bruno.gabriel.fernandes@gmail.com>
2021-06-27 12:07:16 +0800

    Removes `__` and `locale` from nunjucks globals
    They are actually not needed because nunjucks has access to express res.locals, and the i18n middleware populates those based on the request's query param or cookie.

--- src/config/initializers/nunjucksInitializer.ts
+++ src/config/initializers/nunjucksInitializer.ts
@@ -4,7 +4,5 @@ import i18n from 'i18n';
 
 export default function (app: express.Application):void {
   nunjucks.configure(__dirname + '/../../views', { express: app })
-  .addGlobal('__', i18n.__)
-  .addGlobal('locale', i18n.getLocale())
   .addGlobal('locales', i18n.getLocales());
 }
