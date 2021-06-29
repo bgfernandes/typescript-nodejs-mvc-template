549620fb7ee495ef99bae4d6cc9ddbd322fc4eff
Bruno Fernandes <bruno.gabriel.fernandes@gmail.com>
2021-06-29 14:48:15 +0800

    Adds login with google placeholder button and route

--- src/controllers/loginController.ts
+++ src/controllers/loginController.ts
@@ -3,3 +3,7 @@ import express from 'express';
 export function index(_req: express.Request, res: express.Response): void {
   res.render('login/index.njk');
 }
+
+export function loginWithGoogle(_req: express.Request, res: express.Response): void {
+  res.send('placeholder');
+}
