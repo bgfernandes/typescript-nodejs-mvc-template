ba8cf7626ec794989ed5bddd67ffe0c99461ae4b
Bruno Fernandes <bruno.gabriel.fernandes@gmail.com>
2021-06-30 09:31:04 +0800

    Adds /users/me page that shows info on logged in user

--- src/config/routes.ts
+++ src/config/routes.ts
@@ -7,6 +7,7 @@ const webRoutes = express.Router();
 webRoutes.get('/', homeController.index);
 
 webRoutes.get('/users', usersController.index);
+webRoutes.get('/users/me', usersController.showCurrentUser);
 
 webRoutes.get('/login', loginController.index);
 webRoutes.use('/login/google', loginController.googleLoginRouter);
--- src/controllers/usersController.ts
+++ src/controllers/usersController.ts
@@ -5,3 +5,7 @@ export async function index(_req: express.Request, res: express.Response): Promi
   const users = await User.query();
   res.render('users/index.njk', { users });
 }
+
+export function showCurrentUser(req: express.Request, res: express.Response): void {
+  res.render('users/show.njk', { user: req.user });
+}
